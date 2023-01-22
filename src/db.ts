1a8e30cb90ca45034ad0d19c505cdc823931507b
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-01-22 16:50:58 +0900

    4.5 useParams
    URL으로부터 정보를 얻어올수 있는 useParams

---
+++ src/db.ts
@@ -0,0 +1,10 @@
+export const users = [
+  {
+    id: 1,
+    name: "kang",
+  },
+  {
+    id: 2,
+    name: "hyun",
+  },
+];
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import { users } from "../db";
+
 function Home() {
-  //users는 빈배열, 일부러 컴포넌트 충돌이 나도록 만들기
-  const users: any = [];
-  return <h1>{users[0].name}</h1>;
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
