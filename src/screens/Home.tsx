10e6750f48522013b4826c31f673bd5870d3185c
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-01-23 15:13:28 +0900

    4.8 Extras
    useSearchParams은 파라미터를 수정하고 읽을 수 있게 도와준다.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { users } from "../db";
 
 function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams();
+  //http://localhost:3000/?geo=123
+  console.log(readSearchParams.has("geo")); //true
+  console.log(readSearchParams.get("geo")); //123
+  setTimeout(() => {
+    setSearchParams({
+      day: "today",
+      tomorrow: "123",
+    });
+  }, 3000);
   return (
     <div>
       <h1>Users</h1>
