07850200625eac7367aa0e841d1a8b7ca02f8315
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-02-01 16:38:24 +0900

    6.7 Form Validation
    데이터의 validation이 해결되고, 에러가 어디서 발생했는지도 알 수 있음

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,13 +2,44 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  //console.log(register("toDo")); //객체를 반환해준다.
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  console.log(formState.errors);
+  const onValid = (data: any) => {
+    console.log(data);
+  };
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstName", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password1", { required: true, minLength: 5 })}
+          placeholder="Password1"
+        />
+        <input
+          {...register("password2", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short",
+            },
+          })}
+          placeholder="Password2"
+        />
         <button>Add</button>
       </form>
     </div>
