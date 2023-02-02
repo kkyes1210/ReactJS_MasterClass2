28384b222f381d3bfac8e5f7f321ebb095810339
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-02-02 22:50:59 +0900

    6.14 Immutability part One
    수정하고자 하는 todo의 경로 찾기(targetIndex)

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -3,24 +3,23 @@ import { IToDo, toDoState } from "../atom";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
-
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    //console.log("i wanna go ", event.currentTarget.name);
     const {
       currentTarget: { name },
     } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      //console.log(targetIndex);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      console.log(oldToDo, newToDo);
+      return oldToDos;
+    });
   };
-
-  /* const onClick = (newCategory: IToDo["category"]) => {
-    console.log("I wanna to ", newCategory);
-  }; */
 
   return (
     <li>
       <span>{text}</span>
-      {/*  {category !== "DOING" && (
-        <button onClick={() => onClick("DOING")}>Doing</button>
-      )} */}
       {category !== "DOING" && (
         <button name="DOING" onClick={onClick}>
           Doing
