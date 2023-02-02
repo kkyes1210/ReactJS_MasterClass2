30d20d6b7bcded2e8a31a4ada0a96927b8e570d1
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-02-02 22:18:50 +0900

    6.13 Categories
    1. 새 익명함수를 만들어서 인자 전달하기
    2. 버튼에 name을 지정해주고 인자 전달 없이 함수 쓰기

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,41 @@
-import { IToDo } from "../atom";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atom";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    //console.log("i wanna go ", event.currentTarget.name);
+    const {
+      currentTarget: { name },
+    } = event;
+  };
+
+  /* const onClick = (newCategory: IToDo["category"]) => {
+    console.log("I wanna to ", newCategory);
+  }; */
+
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Done</button>
-      <button>Doing</button>
+      {/*  {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )} */}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TODO" && (
+        <button name="TODO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
