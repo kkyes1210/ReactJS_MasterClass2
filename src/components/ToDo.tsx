3aeafa519bc65e619c24da040873727cbaca0034
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-02-03 00:52:33 +0900

    6.15 Immutability part Two
    1. targetIndex 구하기
    2. 배열을 두 부분으로 나누어 새 배열을 만든다.

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,6 +1,14 @@
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atom";
 
+//const food = ["pizza", "mango", "kimchi", "kimbab"];
+// mango를 apple로 교체해보기
+// 1. mango index 구하기
+// 2. 배열을 두 부분으로 나누기
+//const front = ["pizza"];
+//const back = ["kimchi", "kimbab"];
+//const finalPart = [...front, "apple", ...back];
+
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -9,11 +17,12 @@ function ToDo({ text, category, id }: IToDo) {
     } = event;
     setToDos((oldToDos) => {
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      //console.log(targetIndex);
-      const oldToDo = oldToDos[targetIndex];
-      const newToDo = { text, id, category: name };
-      console.log(oldToDo, newToDo);
-      return oldToDos;
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
     });
   };
 
