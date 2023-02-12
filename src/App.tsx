245301732308d34e060a80537fcb4e092b836857
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-02-13 02:24:35 +0900

    7.1 Set Selectors
    set selector

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
-    //event.currentTarget.value는 지금 string인데, 앞에 "+"를 써주면 number타입이 된다.
   };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
+
   return (
     <div>
       <input
@@ -16,7 +19,12 @@ function App() {
         type="number"
         placeholder="Minutes"
       />
-      <input value={hours} type="number" placeholder="Hours" />
+      <input
+        onChange={onHoursChange}
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
