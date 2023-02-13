ec3aa455a6ca7b5ebb3a8121b810983eb26501eb
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-02-14 00:22:04 +0900

    7.7 Performance
    react memo는 react.js에게 특정 component를 렌더링 하지 못하도록 한다.

---
+++ src/Components/DragabbleCard.tsx
@@ -0,0 +1,32 @@
+import { Draggable } from "@hello-pangea/dnd";
+import styled from "styled-components";
+import React from "react";
+
+const Card = styled.div`
+  background-color: ${(props) => props.theme.cardColor};
+  border-radius: 5px;
+  padding: 10px 10px;
+  margin-bottom: 5px;
+`;
+interface IDragabbleCardProps {
+  toDo: string;
+  index: number;
+}
+
+function DragabbleCard({ toDo, index }: IDragabbleCardProps) {
+  console.log(toDo, "has been rendered");
+  return (
+    <Draggable draggableId={toDo} index={index} key={toDo}>
+      {(magic) => (
+        <Card
+          ref={magic.innerRef}
+          {...magic.draggableProps}
+          {...magic.dragHandleProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+export default React.memo(DragabbleCard);
