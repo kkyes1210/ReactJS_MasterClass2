35f4a99abc34dd58cdab86d9a8d4f7eb3512e0ce
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-02-13 23:32:32 +0900

    7.6 Reordering part Two
    1) Delete item on source.index
    2) Put back the item on the destination.index

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,17 @@ const Card = styled.div`
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = ({ source, destination }: DropResult) => {};
+  const onDragEnd = ({ draggableId, source, destination }: DropResult) => {
+    if (!destination) return;
+    setToDos((oldToDos) => {
+      const copyToDos = [...oldToDos];
+      // 1) Delete item on source.index
+      copyToDos.splice(source.index, 1);
+      // 2) Put back the item on the destination.index
+      copyToDos.splice(destination?.index, 0, draggableId);
+      return copyToDos;
+    });
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
@@ -50,7 +60,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable draggableId={toDo} index={index} key={index}>
+                  <Draggable draggableId={toDo} index={index} key={toDo}>
                     {(magic) => (
                       <Card
                         ref={magic.innerRef}
