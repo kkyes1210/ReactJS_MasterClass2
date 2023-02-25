1ea715c4406533d634c1efe016cd3388c2e76a5b
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-02-26 00:56:47 +0900

    8.9 MotionValues part Three
    useScroll로 스크롤 정보를 알 수 있다.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import {
   useMotionValue,
   useMotionValueEvent,
   useTransform,
+  useScroll,
 } from "framer-motion";
 import { useRef, useEffect } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh; //스크롤 할 때 충분한 크기가 아니여서 길이를 늘임
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -26,14 +27,28 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
-  useMotionValueEvent(scale, "change", (x) => {
-    console.log(x);
-  });
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(58, 194, 92), #d2bf5e)",
+    ]
+  );
+
+  const { scrollY, scrollYProgress } = useScroll();
+  useEffect(() => {
+    scrollY.onChange(() => {
+      console.log(scrollY.get(), scrollYProgress.get());
+    });
+  }, [scrollY, scrollYProgress]);
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
   return (
-    <Wrapper>
-      <Box style={{ x: x, scale: scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
