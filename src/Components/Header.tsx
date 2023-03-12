f70e9585f4abeb9b3c868ea5c4ac77ba219339a1
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-03-13 03:43:33 +0900

    9.3 Header part Three
    circle animation
    search bar animation

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { Link, useMatch } from "react-router-dom";
+import { useState } from "react";
 
 const Nav = styled.nav`
   display: flex;
@@ -49,7 +50,7 @@ const Item = styled.li`
   }
 `;
 
-const Circle = styled.span`
+const Circle = styled(motion.span)`
   position: absolute;
   width: 5px;
   height: 5px;
@@ -63,9 +64,12 @@ const Circle = styled.span`
 
 const Search = styled.span`
   color: white;
+  display: flex;
+  align-items: center;
   svg {
     height: 25px;
   }
+  position: relative;
 `;
 
 const logoVariants = {
@@ -74,18 +78,31 @@ const logoVariants = {
   },
   active: {
     fillOpacity: [0, 1, 0],
-    //fillOpacity: [0, 0.4, 0.2, 1, 0.7, 0] 처럼 배열로 선언 가능
     transition: {
       repeat: 5,
-      //무한으로 반복하려면 repeat: Infinity 를 작성하면 된다.
     },
   },
 };
 
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  right: 0px;
+  padding: 5px 10px;
+  padding-left: 40px;
+  z-index: -1;
+  color: white;
+  font-size: 16px;
+  background-color: transparent;
+  border: 1px solid ${(props) => props.theme.white.lighter};
+`;
+
 function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useMatch("/");
   const tvMatch = useMatch("tv");
-  console.log(homeMatch, tvMatch);
+  const toggleSearch = () => setSearchOpen((prev) => !prev);
+
   return (
     <Nav>
       <Col>
@@ -103,21 +120,24 @@ function Header() {
         <Items>
           <Item>
             <Link to="/">
+              {homeMatch && <Circle layoutId="circle" />}
               Home
-              {homeMatch && <Circle />}
             </Link>
           </Item>
           <Item>
             <Link to="tv">
               Tv Shows
-              {tvMatch && <Circle />}
+              {tvMatch && <Circle layoutId="circle" />}
             </Link>
           </Item>
         </Items>
       </Col>
       <Col>
         <Search>
-          <svg
+          <motion.svg
+            onClick={toggleSearch}
+            animate={{ x: searchOpen ? -210 : 0 }}
+            transition={{ type: "linear" }}
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -127,7 +147,12 @@ function Header() {
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
               clipRule="evenodd"
             ></path>
-          </svg>
+          </motion.svg>
+          <Input
+            transition={{ type: "linear" }}
+            animate={{ scaleX: searchOpen ? 1 : 0 }}
+            placeholder="Search for movie or tv show..."
+          />
         </Search>
       </Col>
     </Nav>
